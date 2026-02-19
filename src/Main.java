1c5e95e0d0e74457eb027a9c93829d444702e7f1
ballpskch <ballpskch@gmail.com>
2026-02-19 16:28:57 -0500

    p2.1 combines enlargedCursor, areaHitbox, yellowSquare, restrictMovement, aKeyClick

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 import processing.core.PApplet;
 
-
 public class Main extends PApplet
 {
-    //when in doubt, consult the Processsing reference: https://processing.org/reference/
+    int margin = 200;
+    final int padding = 50;
+    final int buttonSize = 40;
 
-    int margin = 200; //set the margin around the squares
-    final int padding = 50; // padding between buttons and also their width/height
-    final int buttonSize = 40; // padding between buttons and also their width/height
-    ArrayList<Integer> trials = new ArrayList<Integer>(); //contains the order of buttons that activate in the test
-    int trialNum = 0; //the current trial number (indexes into trials array above)
-    int startTime = 0; // time starts when the first click is captured
-    int finishTime = 0; //records the time of the final click
-    int hits = 0; //number of successful clicks
-    int misses = 0; //number of missed clicks
-    Robot robot; //initialized in setup
+    ArrayList<Integer> trials = new ArrayList<Integer>();
+    int trialNum = 0;
+    int startTime = 0;
+    int finishTime = 0;
+    int hits = 0;
+    int misses = 0;
+    Robot robot;
 
-    int numRepeats = 1; //sets the number of times each button repeats in the test
+    int numRepeats = 1;
 
+    // === Enlarged cursor hitbox ===
+    final int cursorRadius = 35;   // visual + hit radius (pixels)
+    final int overlapStep = 2;     // sampling step (smaller = more accurate, slower)
+
+    // === NEW: virtual cursor we clamp inside the allowed area ===
+    float vMouseX, vMouseY;
+    int prevRawMouseX, prevRawMouseY;
+
+    // === NEW: clamp area (border around grid) ===
+    final int clampPad = 25; // how much extra space around the 4x4 grid we allow
+    Rectangle clampRect;
 
     public static void main(String[] args) {
         PApplet.main("Main");
@@ -30,49 +39,69 @@ public class Main extends PApplet
 
     @Override
     public void settings() {
-        size(700,700); // set the size of the window
+        size(700,700);
     }
 
     public void setup()
     {
-        //noCursor(); // hides the system cursor if you want
-        noStroke(); //turn off all strokes, we're just using fills here (can change this if you want)
-        textFont(createFont("Arial",16)); //sets the font to Arial size 16
+        noCursor();          // hide system cursor so our clamped virtual cursor is what you “feel”
+        noStroke();
+        textFont(createFont("Arial",16));
         textAlign(CENTER);
-        frameRate(60); //normally you can't go much higher than 60 FPS.
-        ellipseMode(CENTER); //ellipses are drawn from the center (BUT RECTANGLES ARE NOT!)
-        //rectMode(CENTER); //enabling will break the scaffold code, but you might find it easier to work with centered rects
+        frameRate(60);
+        ellipseMode(CENTER);
 
         try {
-            robot = new Robot(); //create a "Java Robot" class that can move the system cursor
+            robot = new Robot();
         } catch (AWTException e) {
             e.printStackTrace();
         }
 
         //===DON'T MODIFY MY RANDOM ORDERING CODE==
-        for (int i = 0; i < 16; i++) //generate list of targets and randomize the order
-            // number of buttons in 4x4 grid
+        for (int i = 0; i < 16; i++)
             for (int k = 0; k < numRepeats; k++)
-                // number of times each button repeats
                 trials.add(i);
 
-        Collections.shuffle(trials); // randomize the order of the buttons
-        System.out.println("trial order: " + trials); //print out order for reference
+        Collections.shuffle(trials);
+        System.out.println("trial order: " + trials);
 
-        surface.setLocation(0,0);// put window in top left corner of screen (doesn't always work)
+        surface.setLocation(0,0);
+
+        // Compute clamp rectangle around the 4x4 grid
+        int gridW = 3 * (padding + buttonSize) + buttonSize;
+        int gridH = 3 * (padding + buttonSize) + buttonSize;
+        clampRect = new Rectangle(margin - clampPad, margin - clampPad,
+                gridW + 2 * clampPad, gridH + 2 * clampPad);
+
+        // Start virtual cursor at center of grid
+        vMouseX = margin + gridW / 2f;
+        vMouseY = margin + gridH / 2f;
+
+        prevRawMouseX = mouseX;
+        prevRawMouseY = mouseY;
     }
-
 
     public void draw()
     {
-        background(0); //set background to black
+        background(0);
 
-        if (trialNum >= trials.size()) //check to see if test is over
+        // Update virtual cursor using real mouse deltas, then clamp inside clampRect
+        int dx = mouseX - prevRawMouseX;
+        int dy = mouseY - prevRawMouseY;
+        prevRawMouseX = mouseX;
+        prevRawMouseY = mouseY;
+
+        vMouseX += dx;
+        vMouseY += dy;
+
+        vMouseX = constrain(vMouseX, clampRect.x, clampRect.x + clampRect.width);
+        vMouseY = constrain(vMouseY, clampRect.y, clampRect.y + clampRect.height);
+
+        if (trialNum >= trials.size())
         {
             float timeTaken = (finishTime-startTime) / 1000f;
             float penalty = constrain(((95f-((float)hits*100f/(float)(hits+misses)))*.2f),0,100);
-            fill(255); //set fill color to white
-            //write to screen (not console)
+            fill(255);
             text("Finished!", width / 2, height / 2);
             text("Hits: " + hits, width / 2, height / 2 + 20);
             text("Misses: " + misses, width / 2, height / 2 + 40);
@@ -80,92 +109,188 @@ public class Main extends PApplet
             text("Total time taken: " + timeTaken + " sec", width / 2, height / 2 + 80);
             text("Average time for each button: " + nf((timeTaken)/(float)(hits+misses),0,3) + " sec", width / 2, height / 2 + 100);
             text("Average time for each button + penalty: " + nf(((timeTaken)/(float)(hits+misses) + penalty),0,3) + " sec", width / 2, height / 2 + 140);
-            return; //return, nothing else to do now test is over
-        }
-
-        fill(255); //set fill color to white
-        text((trialNum + 1) + " of " + trials.size(), 40, 20); //display what trial the user is on
-
-        for (int i = 0; i < 16; i++)// for all button
-            drawButton(i); //draw button
-
-        fill(255, 0, 0, 200); // set fill color to translucent red
-        ellipse(mouseX, mouseY, 20, 20); //draw user cursor as a circle with a diameter of 20
-
-    }
-
-    public void mousePressed() // test to see if hit was in target!
-    {
-        if (trialNum >= trials.size()) //check if task is done
             return;
-
-        if (trialNum == 0) //check if first click, if so, record start time
-            startTime = millis();
-
-        if (trialNum == trials.size() - 1) //check if final click
-        {
-            finishTime = millis();
-            //write to terminal some output:
-            System.out.println("we're all done!");
         }
 
-        Rectangle bounds = getButtonLocation(trials.get(trialNum));
+        fill(255);
+        text((trialNum + 1) + " of " + trials.size(), 60, 20);
+        text("Click mouse or press 'A' to click. Cursor is clamped near grid.", width / 2, 20);
 
-        //check to see if cursor was inside button
-        if ((mouseX > bounds.x && mouseX < bounds.x + bounds.width) && (mouseY > bounds.y && mouseY < bounds.y + bounds.height)) // test to see if hit was within bounds
-        {
-            System.out.println("HIT! " + trialNum + " " + (millis() - startTime)); // success
-            hits++;
-        } else
-        {
-            System.out.println("MISSED! " + trialNum + " " + (millis() - startTime)); // fail
-            misses++;
-        }
+        // Draw buttons
+        for (int i = 0; i < 16; i++)
+            drawButton(i);
 
-        trialNum++; // Increment trial number
+        // Draw the clamp border so it’s visible
+        drawClampBorder();
 
-        //in this example design, I move the cursor back to the middle after each click
-        //robot.mouseMove(width/2, (height)/2); //on click, move cursor to roughly center of window!
+        // Draw enlarged cursor at virtual cursor position
+        drawBigCursor((int)vMouseX, (int)vMouseY);
     }
 
-    //probably shouldn't have to edit this method
-    public Rectangle getButtonLocation(int i) //for a given button ID, what is its location and size
+    public void mousePressed()
     {
-        int x = (i % 4) * (padding + buttonSize) + margin;
-        int y = (i / 4) * (padding + buttonSize) + margin;
-
-        return new Rectangle(x, y, buttonSize, buttonSize);
-    }
-
-    //you can edit this method to change how buttons appear
-    public void drawButton(int i)
-    {
-        Rectangle bounds = getButtonLocation(i);
-
-        if (trials.get(trialNum) == i) // see if current button is the target
-            fill(0, 255, 255); // if so, fill cyan
-        else
-            fill(200); // if not, fill gray
-
-        rect(bounds.x, bounds.y, bounds.width, bounds.height);
-    }
-
-    public void mouseMoved()
-    {
-        //can do stuff everytime the mouse is moved (i.e., not clicked)
-        //https://processing.org/reference/mouseMoved_.html
-    }
-
-    public void mouseDragged()
-    {
-        //can do stuff everytime the mouse is dragged
-        //https://processing.org/reference/mouseDragged_.html
+        performClick();
     }
 
     public void keyPressed()
     {
-        //can use the keyboard if you wish
-        //https://processing.org/reference/keyTyped_.html
-        //https://processing.org/reference/keyCode.html
+        // Allow using the "A" key to click
+        if (key == 'a' || key == 'A') {
+            performClick();
+        }
     }
+
+    // Shared click logic for mouse and 'A' key
+    private void performClick()
+    {
+        if (trialNum >= trials.size())
+            return;
+
+        if (trialNum == 0)
+            startTime = millis();
+
+        if (trialNum == trials.size() - 1)
+        {
+            finishTime = millis();
+            System.out.println("we're all done!");
+        }
+
+        // Choose button covered MOST by the cursor hitbox (using virtual cursor)
+        int chosen = getMostCoveredButton((int)vMouseX, (int)vMouseY);
+
+        if (chosen == -1) {
+            System.out.println("MISSED! (no overlap) " + trialNum + " " + (millis() - startTime));
+            misses++;
+            trialNum++;
+            return;
+        }
+
+        int target = trials.get(trialNum);
+
+        if (chosen == target)
+        {
+            System.out.println("HIT! (chosen=" + chosen + ") " + trialNum + " " + (millis() - startTime));
+            hits++;
+        } else
+        {
+            System.out.println("MISSED! (chosen=" + chosen + ", target=" + target + ") " + trialNum + " " + (millis() - startTime));
+            misses++;
+        }
+
+        trialNum++;
+    }
+
+    public Rectangle getButtonLocation(int i)
+    {
+        int x = (i % 4) * (padding + buttonSize) + margin;
+        int y = (i / 4) * (padding + buttonSize) + margin;
+        return new Rectangle(x, y, buttonSize, buttonSize);
+    }
+
+    public void drawButton(int i)
+    {
+        Rectangle bounds = getButtonLocation(i);
+
+        // === Yellow Target Square (instead of blue) ===
+        if (trials.get(trialNum) == i)
+            fill(255, 255, 0); // yellow
+        else
+            fill(200);
+
+        rect(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+
+    private void drawClampBorder() {
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(clampRect.x, clampRect.y, clampRect.width, clampRect.height);
+        noStroke();
+        strokeWeight(1);
+    }
+
+    private void drawBigCursor(int cx, int cy) {
+        // translucent fill showing hit area
+        noStroke();
+        fill(255, 0, 0, 60);
+        ellipse(cx, cy, cursorRadius * 2, cursorRadius * 2);
+
+        // ring outline
+        noFill();
+        stroke(255, 0, 0, 220);
+        strokeWeight(3);
+        ellipse(cx, cy, cursorRadius * 2, cursorRadius * 2);
+
+        // center dot
+        noStroke();
+        fill(255, 0, 0, 230);
+        ellipse(cx, cy, 8, 8);
+
+        noStroke();
+        strokeWeight(1);
+    }
+
+    // Returns button index 0..15, or -1 if overlap is zero for all.
+    private int getMostCoveredButton(int cx, int cy) {
+        int bestIdx = -1;
+        int bestScore = 0;
+
+        int r = cursorRadius;
+        int r2 = r * r;
+
+        int minX = cx - r;
+        int maxX = cx + r;
+        int minY = cy - r;
+        int maxY = cy + r;
+
+        for (int i = 0; i < 16; i++) {
+            Rectangle b = getButtonLocation(i);
+
+            // quick reject
+            if (maxX < b.x || minX > b.x + b.width || maxY < b.y || minY > b.y + b.height)
+                continue;
+
+            int score = 0;
+
+            // sample only overlapping bbox region
+            int sx0 = max(minX, b.x);
+            int sx1 = min(maxX, b.x + b.width);
+            int sy0 = max(minY, b.y);
+            int sy1 = min(maxY, b.y + b.height);
+
+            for (int x = sx0; x <= sx1; x += overlapStep) {
+                int dx = x - cx;
+                int dx2 = dx * dx;
+                for (int y = sy0; y <= sy1; y += overlapStep) {
+                    int dy = y - cy;
+                    if (dx2 + dy * dy <= r2) {
+                        score++;
+                    }
+                }
+            }
+
+            if (score > bestScore) {
+                bestScore = score;
+                bestIdx = i;
+            } else if (score == bestScore && score > 0) {
+                // Optional tie-breaker: choose the one whose center is closer to cursor center
+                // (makes edge cases feel more stable)
+                Rectangle best = getButtonLocation(bestIdx);
+                float bestCx = best.x + best.width / 2f;
+                float bestCy = best.y + best.height / 2f;
+                float curBestD = sq(bestCx - cx) + sq(bestCy - cy);
+
+                float thisCx = b.x + b.width / 2f;
+                float thisCy = b.y + b.height / 2f;
+                float thisD = sq(thisCx - cx) + sq(thisCy - cy);
+
+                if (thisD < curBestD) bestIdx = i;
+            }
+        }
+
+        return (bestScore == 0) ? -1 : bestIdx;
+    }
+
+    public void mouseMoved() { }
+    public void mouseDragged() { }
 }
