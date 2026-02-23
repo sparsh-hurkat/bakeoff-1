07fc80e794299db5adb5321fa9a53ba8dd5781ee
sparsh-hurkat <hurkatsparsh@gmail.com>
2026-02-23 18:17:45 -0500

    code correction

--- src/Main.java
+++ src/Main.java
@@ -1,157 +1,178 @@
-/*
-FINAL VERSION — Fitts Law Cursor Reset + Metrics + RED CIRCLE Cursor
-*/
-
-import java.awt.*;
-import java.util.*;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.util.ArrayList;
+import java.util.Collections;
 import processing.core.PApplet;
 
-public class Main extends PApplet {
 
-    int margin = 200;
-    final int padding = 50;
-    final int buttonSize = 40;
+public class Main extends PApplet
+{
+    //when in doubt, consult the Processsing reference: https://processing.org/reference/
 
-    boolean useCustomCursor = true;
+    int margin = 200; //set the margin around the squares
+    final int padding = 50; // padding between buttons and also their width/height
+    final int buttonSize = 40; // padding between buttons and also their width/height
+    ArrayList<Integer> trials = new ArrayList<Integer>(); //contains the order of buttons that activate in the test
+    int trialNum = 0; //the current trial number (indexes into trials array above)
+    int startTime = 0; // time starts when the first click is captured
+    int finishTime = 0; //records the time of the final click
+    int hits = 0; //number of successful clicks
+    int misses = 0; //number of missed clicks
+    Robot robot; //initialized in setup
 
-    ArrayList<Integer> trials = new ArrayList<>();
+    int numRepeats = 2; //sets the number of times each button repeats in the test
 
-    int trialNum = 0;
-    int hits = 0;
-    int misses = 0;
 
-    long startTime = 0;
-    long finishTime = 0;
-
-    boolean finished = false;
-
-    Robot robot;
-
-    public static void main(String[] args){ PApplet.main("Idea1"); }
-    public void settings(){ size(700,700); }
-
-    public void setup(){
-        noStroke();
-        try { robot = new Robot(); } catch(Exception e){ e.printStackTrace(); }
-
-        for(int i=0;i<16;i++) trials.add(i);
-        Collections.shuffle(trials);
-
-        if(useCustomCursor) noCursor();
-        moveCursorToGridCenter();
+    public static void main(String[] args) {
+        PApplet.main("Main");
     }
 
-    public void draw(){
-        background(0);
+    @Override
+    public void settings() {
+        size(700,700); // set the size of the window
+    }
 
-        if(!finished){
-            drawButtons();
-            drawTopScore();
-        } else {
-            drawResults();
+    public void setup()
+    {
+        //noCursor(); // hides the system cursor if you want
+        noStroke(); //turn off all strokes, we're just using fills here (can change this if you want)
+        textFont(createFont("Arial",16)); //sets the font to Arial size 16
+        textAlign(CENTER);
+        frameRate(60); //normally you can't go much higher than 60 FPS.
+        ellipseMode(CENTER); //ellipses are drawn from the center (BUT RECTANGLES ARE NOT!)
+        //rectMode(CENTER); //enabling will break the scaffold code, but you might find it easier to work with centered rects
+
+        try {
+            robot = new Robot(); //create a "Java Robot" class that can move the system cursor
+        } catch (AWTException e) {
+            e.printStackTrace();
         }
 
-        drawCustomCursor();
+        //===DON'T MODIFY MY RANDOM ORDERING CODE==
+        for (int i = 0; i < 16; i++) //generate list of targets and randomize the order
+            // number of buttons in 4x4 grid
+            for (int k = 0; k < numRepeats; k++)
+                // number of times each button repeats
+                trials.add(i);
+
+        Collections.shuffle(trials); // randomize the order of the buttons
+        System.out.println("trial order: " + trials); //print out order for reference
+
+        surface.setLocation(0,0);// put window in top left corner of screen (doesn't always work)
     }
 
-    void drawButtons(){
-        for(int i=0;i<16;i++){
-            Rectangle b = getButtonLocation(i);
 
-            if(trials.get(trialNum) == i) fill(0,255,255);
-            else fill(120);
+    public void draw()
+    {
+        background(0); //set background to black
 
-            rect(b.x,b.y,b.width,b.height);
+        if (trialNum >= trials.size()) //check to see if test is over
+        {
+            float timeTaken = (finishTime-startTime) / 1000f;
+            float penalty = constrain(((95f-((float)hits*100f/(float)(hits+misses)))*.2f),0,100);
+            fill(255); //set fill color to white
+            //write to screen (not console)
+            text("Finished!", width / 2, height / 2);
+            text("Hits: " + hits, width / 2, height / 2 + 20);
+            text("Misses: " + misses, width / 2, height / 2 + 40);
+            text("Accuracy: " + (float)hits*100f/(float)(hits+misses) +"%", width / 2, height / 2 + 60);
+            text("Total time taken: " + timeTaken + " sec", width / 2, height / 2 + 80);
+            text("Average time for each button: " + nf((timeTaken)/(float)(hits+misses),0,3) + " sec", width / 2, height / 2 + 100);
+            text("Average time for each button + penalty: " + nf(((timeTaken)/(float)(hits+misses) + penalty),0,3) + " sec", width / 2, height / 2 + 140);
+            return; //return, nothing else to do now test is over
         }
+
+        fill(255); //set fill color to white
+        text((trialNum + 1) + " of " + trials.size(), 40, 20); //display what trial the user is on
+
+        for (int i = 0; i < 16; i++)// for all button
+            drawButton(i); //draw button
+
+        // draw bullseye cursor
+        fill(255, 0, 0, 220);    // red outer ring
+        ellipse(mouseX, mouseY, 30, 30);
+        fill(255, 255, 255, 220); // white middle ring
+        ellipse(mouseX, mouseY, 20, 20);
+        fill(255, 0, 0, 220);    // red inner ring
+        ellipse(mouseX, mouseY, 12, 12);
+        fill(255, 255, 255, 220); // white center dot
+        ellipse(mouseX, mouseY, 4, 4);
+
     }
 
-    void drawTopScore(){
-        fill(255);
-        textAlign(LEFT, TOP);
-        textSize(16);
-        text("Correct: " + hits + " / 16", 20, 20);
-        text("Misses: " + misses, 20, 45);
-    }
+    public void mousePressed() // test to see if hit was in target!
+    {
+        if (trialNum >= trials.size()) //check if task is done
+            return;
 
-    public void mousePressed(){
-
-        if(finished) return;
-
-        // start timer on first click
-        if(hits == 0 && misses == 0){
+        if (trialNum == 0) //check if first click, if so, record start time
             startTime = millis();
+
+        if (trialNum == trials.size() - 1) //check if final click
+        {
+            finishTime = millis();
+            //write to terminal some output:
+//            System.out.println("we're all done!");
         }
 
-        Rectangle b = getButtonLocation(trials.get(trialNum));
+        Rectangle bounds = getButtonLocation(trials.get(trialNum));
 
-        if(mouseX > b.x && mouseX < b.x + b.width &&
-                mouseY > b.y && mouseY < b.y + b.height){
-
+        //check to see if cursor was inside button
+        if ((mouseX > bounds.x && mouseX < bounds.x + bounds.width) && (mouseY > bounds.y && mouseY < bounds.y + bounds.height)) // test to see if hit was within bounds
+        {
+            System.out.println("HIT! " + trialNum + "; " + trials.get(trialNum) + "; " + (millis() - startTime)); // success
             hits++;
-            trialNum++;
-
-            if(hits == 16){
-                finishTime = millis();
-                finished = true;
-            }
-
-        } else {
+        } else
+        {
+            System.out.println("MISSED! " + trialNum + "; " + trials.get(trialNum) + (millis() - startTime)); // fail
             misses++;
         }
 
-        if(!finished) moveCursorToGridCenter();
+        trialNum++; // Increment trial number
+
+        //in this example design, I move the cursor back to the middle after each click
+        //robot.mouseMove(width/2, (height)/2); //on click, move cursor to roughly center of window!
     }
 
-    void drawResults(){
-
-        fill(255);
-        textAlign(CENTER, CENTER);
-        textSize(18);
-
-        float rawTime = (finishTime - startTime) / 1000.0f;
-        float penaltyTime = misses * 0.5f;
-        float finalTime = rawTime + penaltyTime;
-        float avgTimePerButton = rawTime / 16.0f;
-
-        text("Finished!", width/2, height/2 - 120);
-        text("Correct Clicks: " + hits, width/2, height/2 - 80);
-        text("Misclicks: " + misses, width/2, height/2 - 50);
-
-        text("Raw Time: " + nf(rawTime,1,2) + " s", width/2, height/2 - 10);
-        text("Penalty Time: " + nf(penaltyTime,1,2) + " s", width/2, height/2 + 20);
-        text("Final Adjusted Time: " + nf(finalTime,1,2) + " s", width/2, height/2 + 50);
-        text("Avg Time per Button: " + nf(avgTimePerButton,1,2) + " s", width/2, height/2 + 90);
-    }
-
-    Rectangle getButtonLocation(int i){
+    //probably shouldn't have to edit this method
+    public Rectangle getButtonLocation(int i) //for a given button ID, what is its location and size
+    {
         int x = (i % 4) * (padding + buttonSize) + margin;
         int y = (i / 4) * (padding + buttonSize) + margin;
+
         return new Rectangle(x, y, buttonSize, buttonSize);
     }
 
-    void moveCursorToGridCenter() {
-        int gridCenterX = margin + (4 * buttonSize + 3 * padding) / 2;
-        int gridCenterY = margin + (4 * buttonSize + 3 * padding) / 2;
+    //you can edit this method to change how buttons appear
+    public void drawButton(int i)
+    {
+        Rectangle bounds = getButtonLocation(i);
 
-        Point windowLoc = ((java.awt.Canvas) surface.getNative()).getLocationOnScreen();
-        int screenX = windowLoc.x + gridCenterX;
-        int screenY = windowLoc.y + gridCenterY;
+        if (trials.get(trialNum) == i) // see if current button is the target
+            fill(0, 255, 255); // if so, fill cyan
+        else
+            fill(200); // if not, fill gray
 
-        robot.mouseMove(screenX, screenY);
+        rect(bounds.x, bounds.y, bounds.width, bounds.height);
     }
 
-    // BIG RED CIRCLE CURSOR
-    void drawCustomCursor(){
-        if(!useCustomCursor) return;
+    public void mouseMoved()
+    {
+        //can do stuff everytime the mouse is moved (i.e., not clicked)
+        //https://processing.org/reference/mouseMoved_.html
+    }
 
-        noStroke();
-        fill(255,0,0,60);   // glow ring
-        ellipse(mouseX, mouseY, 40, 40);
+    public void mouseDragged()
+    {
+        //can do stuff everytime the mouse is dragged
+        //https://processing.org/reference/mouseDragged_.html
+    }
 
-        fill(255,0,0);      // main cursor
-        ellipse(mouseX, mouseY, 16, 16);
-
-        fill(255);          // precision center dot
-        ellipse(mouseX, mouseY, 4, 4);
+    public void keyPressed()
+    {
+        //can use the keyboard if you wish
+        //https://processing.org/reference/keyTyped_.html
+        //https://processing.org/reference/keyCode.html
     }
 }
